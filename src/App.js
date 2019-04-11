10c68b1ae060ec24776f314ec1f890b80eb02a21
arunrk <arunrk.cortasys@gmail.com>
2019-04-11 12:57:44 +0530

    single page apps

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+//import First from './First'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+         <h1>Sample Page</h1>
+         {/* <First subject='python' dept="mca" college="ksrcas" /> */}
+      </div>
+    );
+  }
+}
+
+export default App;
