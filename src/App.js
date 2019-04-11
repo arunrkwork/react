abc7c13500a3aef232fe71155a39283b7a8099c6
arunrk <arunrk.cortasys@gmail.com>
2019-04-11 13:00:02 +0530

    nested components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-//import First from './First'
+import First from './First';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
          <h1>Sample Page</h1>
-         {/* <First subject='python' dept="mca" college="ksrcas" /> */}
+         <First/>
       </div>
     );
   }
