d8730db121cbcf382fc916d6500766b921f471b5
arunrk <arunrk.cortasys@gmail.com>
2019-04-11 14:24:33 +0530

    conditional operator

--- src/First.js
+++ src/First.js
@@ -2,21 +2,63 @@ import React from 'react';
 
 const First = ({students}) => {
   
-        const studentList = students.map(student => {
-            return(
-                <div className="student" key = {student.id}>
-                    <div>Subject : { student.subject } </div>
-                    <div>Dept : { student.dept }</div>
-                    <div>College : { student.college }</div>
-                </div>
-            )
-        })
+        // using if else
 
-        return(
-            <div className="student-list">
-                {studentList}
-            </div>
-        )
+        // const studentList = students.map(student => {
+
+        //     if(student.id > 2) {
+
+        //         return(
+        //             <div className="student" key = {student.id}>
+        //                 <div>Id : { student.id } </div>
+        //                 <div>Subject : { student.subject } </div>
+        //                 <div>Dept : { student.dept }</div>
+        //                 <div>College : { student.college }</div>
+        //             </div>
+        //         )
+
+        //     } else {
+        //         return null
+        //     }
+        // })
+
+        // using ternary operator
+
+        // const studentList = students.map(student => {
+        //    return student.id > 2 ? (
+        //         <div className="student" key = {student.id}>
+        //                  <div>Id : { student.id } </div>
+        //                  <div>Subject : { student.subject } </div>
+        //                  <div>Dept : { student.dept }</div>
+        //                  <div>College : { student.college }</div>
+        //              </div>
+        //     ) : (null)
+        // })
+
+        // return(
+        //     <div className="student-list">
+        //         {studentList}
+        //     </div>
+        // )
+
+        // assing directly to the output
+  
+         return( 
+             <div className="student-list">
+                 {
+                    students.map(student => {
+                        return student.id > 1 ? (
+                            <div className="student" key = {student.id}>
+                                    <div>Id : { student.id } </div>
+                                    <div>Subject : { student.subject } </div>
+                                    <div>Dept : { student.dept }</div>
+                                    <div>College : { student.college }</div>
+                                </div>
+                        ) : (null)
+                    }) 
+                 }
+             </div>
+         )
  
 }
 
