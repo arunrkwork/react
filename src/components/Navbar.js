be54e71c1cb8965b234eb5d750a37acdb8b7c097
arunrk <arunrk.cortasys@gmail.com>
2019-04-17 13:51:02 +0530

    React Router

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
-
     return(
         <nav className="nav-wrapper red darken-3">
             <div className="container">
